5487cf9ab7d46f710a90a2fabc79c44e3df440f9
Natalia Denisova <netcpmoeip@Pros-MacBook-Pro.local>
2022-12-06 11:53:50 +0200

    initial commit

--- js/common.js
+++ js/common.js
@@ -1,8 +1,24 @@
 jQuery(document).ready(function($){
-
-	$('.navigation a').on( 'touchstart click', function() {
-			$( '.card' ).not( '.' + $(this).attr('id') ).hide('fast');
-			$( '.card.' + $(this).attr('id') ).slideDown('fast');
+    $('.navigation a').on( 'touchstart click', function(e) {
+        e.preventDefault();
+        e.stopImmediatePropagation();
+		$( '.card' ).not( '.' + $(this).attr('id') ).hide('fast');
+		$( '.card.' + $(this).attr('id') ).slideDown('fast');
+        var color = $( '.card.' + $(this).attr('id') ).css('border-color');
+        $('.navigation a').css('border', 'none');
+        $(this).css('border-bottom', '.25em solid ' + color);
 	});
+
+    $('h1#index').on('click', function() {
+        $('.navigation').css('border', 'none');
+        $( '.card' ).slideDown('fast');
+    });
+
+    $('#solve').on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $("#form").offset().top
+        }, 500);
+    })
 
 });
